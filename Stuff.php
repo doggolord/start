74b1f68a328896c4808204cce792381cc60715d9
gert-indrek.poljakov <gert-indrek.poljakov@ametikool.ee>
2019-01-24 15:09:10 +0200

    first commit

---
+++ Stuff.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gert-indrek.poljakov
+ * Date: 24.01.2019
+ * Time: 13:27
+ */
+
+class Stuff
+{
+    public $doors = 4;
+    public $doors1 = 4;
+}
+print_r(new Stuff());
+
